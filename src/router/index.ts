ca5ef4047d7dce0d8527fd9b412c073ad94f69d8
Hieu Chu <minhieuchu@gmail.com>
2022-07-31 18:28:52 +0900

    Rename Home component & Remove unnecessary urls

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
-import HomeView from '../views/HomeView.vue';
+import ProjectBoard from '@/views/ProjectBoard.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "ProjectBoard",
+    component: ProjectBoard,
   },
 ];
 
