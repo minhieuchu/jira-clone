c05ae947793385e714724029768b8b2e1d70127b
Hieu Chu <minhieuchu@gmail.com>
2022-08-07 12:23:55 +0900

    Use kanban column data from API call to render kanban board

--- src/components/kanban/KanbanBoard.vue
+++ src/components/kanban/KanbanBoard.vue
@@ -1,37 +1,57 @@
 <template>
-  <div class="kanban-container">
+  <div v-if="!isLoading" class="kanban-container">
     <kanban-column
       v-for="(column, index) in columns"
       :key="index"
-      :status="column"
+      :status="convertColumnStatus(column.status)"
     ></kanban-column>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import KanbanColumn from "./KanbanColumn.vue";
 import { getKanbanColumns } from "@/services/jiraApi/JiraApiColumn";
 import { getKanbanIssues } from "@/services/jiraApi/JiraApiIssue";
 import ColumnModule from "@/store/modules/KanbanColumn";
 import IssueModule from "@/store/modules/KanbanIssue";
+import { IKanbanColumn } from "@/services/interfaces/IKanbanBoard";
 
 export default defineComponent({
   components: {
     KanbanColumn,
   },
-  setup() {
-    getKanbanColumns().then((res) => {
-      ColumnModule.setColumns(res);
-    });
-    getKanbanIssues().then((res) => {
-      IssueModule.setIssues(res);
-    });
-  },
   data() {
     return {
-      columns: ["BACKLOG", "IN PROGRESS", "IN REVIEW", "DONE"],
+      isLoading: true,
+      columns: computed(() => ColumnModule.columns),
     };
+  },
+  created() {
+    // These promises passed to Promise.all will always resolve,
+    // as error is handled in the promise implementation.
+    // In case of error, each promise will resolve with an empty array.
+    Promise.all([getKanbanColumns(), getKanbanIssues()]).then(
+      ([columns, issues]) => {
+        if (!columns.length) {
+          return;
+        }
+        columns.sort((a: IKanbanColumn, b: IKanbanColumn) => {
+          if (a.columnIndex < b.columnIndex) {
+            return -1;
+          }
+          return 1;
+        });
+        ColumnModule.setColumns(columns);
+        IssueModule.setIssues(issues);
+        this.isLoading = false;
+      }
+    );
+  },
+  methods: {
+    convertColumnStatus(status: string): string {
+      return status.toUpperCase().replaceAll("_", " ");
+    },
   },
 });
 </script>
